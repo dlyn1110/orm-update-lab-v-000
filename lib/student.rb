b2a8bfdd8a9784beaee31ce4c9ba12342bd89a06
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 22:04:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,8 @@ class Student
    end
 
    def self.create(name, grade)
-     
-
+     student = Student.new(name, grade)
+     student.save
    end
 
   # Remember, you can access your database connection anywhere in this class
