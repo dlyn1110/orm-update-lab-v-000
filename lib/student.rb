ef80276503c572acda7d7ce2b9da8084aa20852a
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 22:06:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,11 +53,11 @@ class Student
    end
 
    def self.new_from_db(row)
-     new_student = self.new
-  new_student.id = row[0]
-  new_student.name =  row[1]
-  new_student.grade = row[2]
-  new_student
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
    end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
