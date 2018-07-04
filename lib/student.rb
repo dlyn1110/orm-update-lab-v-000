a26a7d577920078fe94b2f67e6740c5e7b0c8a2c
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 22:10:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,7 @@ class Student
     name =  row[1]
     grade = row[2]
     self.new(id, name, grade)
+    binding.pry
    end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
